70356e98dc47bcad9da8ddba5d5c66bea79444d0
Berk <berkteke16@gmail.com>
2023-10-29 04:21:08 +0300

    Update CustomerService.java

--- src/main/java/com/example/teke/ESHOP/service/CustomerService.java
+++ src/main/java/com/example/teke/ESHOP/service/CustomerService.java
@@ -1,5 +1,6 @@
 package com.example.teke.ESHOP.service;
 
+import com.example.teke.ESHOP.config.SecurityConfig;
 import com.example.teke.ESHOP.dto.CustomerDTO;
 import com.example.teke.ESHOP.model.Customer;
 import com.example.teke.ESHOP.repository.CustomerRepository;
@@ -58,10 +59,15 @@ public class CustomerService {
 
     public Boolean login(String username, String password) {
 
+        SecurityConfig securityConfig = new SecurityConfig();
+
         Customer existCustomer = customerRepository.findByUsername(username);
         if (existCustomer.getUsername().equals(username) && existCustomer.getPassword().equals(password)) {
+            
             return true;
         }
         return false;
     }
+
+
 }
